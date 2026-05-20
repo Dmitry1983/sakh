047b961ca40ed211b687545a480dedfd45dd2fec
dibasch <barbash.dv@gmail.com>
2026-05-20 10:52:37 +0300

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from '../store';
 import { Main } from '../screens/Main';
 
+//
 export const App: React.FC = () => {
   return (
     <Provider store={store}>
